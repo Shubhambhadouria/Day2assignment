4502c877a4bf2e5a23be692155466a13be6a40b1
Shubhambhadouria <shubhambhadouria07@gmail.com>
2022-05-18 01:02:33 +0530

    s

--- prime.java
+++ prime.java
@@ -4,19 +4,25 @@ class Prime{
 
 public static void main(String[] args){
 	
-	int c = 12;
-	
-	for(int i = 1; i <= 12; i++ ){
-	
-		if(c%i ==0){
-		
-			System.out.println(i);
+	int c=101;
 
-		}
+	if(c>=2 && c<=100){for(int i = 1; i<=c;i++){
+
+if(c%i==0){
+
+System.out.println(i);
+
+}
+
+}
+
+}
+	else{
+System.out.println("Invalid Number");
+}
 	
-	}
-		
-	}
+	}		
+	
 
 
 }
